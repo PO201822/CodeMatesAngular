16638499cd67de2a0bb89ea592c700cd65651e14
Taraszovics Péter <gtarapeti@gmail.com>
2019-08-01 18:20:39 +0200

    implemented onSubmit method for sending a post request and onSuccessfulLogin to handle the response

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,13 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html'})
 export class LoginFormComponent implements OnInit {
+  model : any = {}
 
-  constructor() { }
+  constructor(
+    private http : HttpClient,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    let url = environment.apiUrl + '/auth/signin';
+    this.http.post<any>(url, {
+      username: this.model.username,
+      password: this.model.password
+  }).subscribe(res => this.onSuccessfulLogin(),
+  error => this.handleError(error)); {
+  };
+}
+
+  onSuccessfulLogin(){
+    this.router.navigate(['home']);
+
+  }
+
+  handleError(error){
+
+
+
   }
 
 }
