1b0b1a20beb52cf345f667238f296c6032c8b2e9
Gergő Papp <imgergopapp@gmail.com>
2019-08-01 09:26:59 +0200

    Removed style.css from login-form component

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -2,9 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login-form',
-  templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.css']
-})
+  templateUrl: './login-form.component.html'})
 export class LoginFormComponent implements OnInit {
 
   constructor() { }
