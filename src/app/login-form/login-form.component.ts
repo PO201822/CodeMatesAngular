2363d2385aca5d5c2caaeb4e19fec955ca6c9dec
Gergő Papp <imgergopapp@gmail.com>
2019-08-12 09:59:28 +0200

    Refactor code

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -9,36 +9,36 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
   templateUrl: './login-form.component.html',
 })
 export class LoginFormComponent implements OnInit {
-  model : any = {}
+  model: any = {}
 
   constructor(
-    private http : HttpClient,
+    private http: HttpClient,
     private router: Router
   ) { }
 
   ngOnInit() {
-    
-}
-onRegisterClicked(){
-  this.router.navigate(['registration']);
-}
+
+  }
+  onRegisterClicked() {
+    this.router.navigate(['registration']);
+  }
 
   onSubmit() {
     let url = environment.apiUrl + '/auth/signin';
     this.http.post<any>(url, {
       name: this.model.username,
       password: this.model.password
-  }).subscribe(res => this.onSuccessfulLogin(),
-  error => this.handleError(error)); {
-  };
-}
+    }).subscribe(res => this.onSuccessfulLogin(),
+      error => this.handleError(error)); {
+    };
+  }
 
-  onSuccessfulLogin(){
+  onSuccessfulLogin() {
     this.router.navigate(['home']);
 
   }
 
-  handleError(error){
+  handleError(error) {
 
 
   }
