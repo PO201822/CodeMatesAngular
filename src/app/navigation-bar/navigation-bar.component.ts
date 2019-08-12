a3d189b9d9dcaeee3bcd541847472582eeee5a66
Gergő Papp <imgergopapp@gmail.com>
2019-08-12 13:08:20 +0200

    Add click event handler methods skeleton

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-navigation-bar',
@@ -6,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
 
+  onProfileClicked() {
+    let url = environment.apiUrl + '/auth/signin';
+    this.http.get<any>(url, {
+      name: this.model.username
+    }).subscribe(res => this.onSuccessfulProfile(),
+      error => this.handleError(error)); {
+    };
+  }
+
+  onLogoutClicked() {
+
+  }
+
+  handleError(error) {
+
+  }
+
+  onSuccessfulProfile() {
+
+  }
 }
