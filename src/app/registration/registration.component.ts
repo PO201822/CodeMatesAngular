a687243952fa5621792698822b81163ca0558c84
Gergő Papp <imgergopapp@gmail.com>
2019-08-12 09:56:11 +0200

    Add onSubmitRegistrationClicked method and model object

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 
@@ -7,16 +9,19 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
   templateUrl: './registration.component.html'
 })
 export class RegistrationComponent implements OnInit {
-  private router: Router
+  model : any = {}
 
 
-  constructor() { }
+  constructor(
+    private http : HttpClient,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
   onBackToRegisterClicked(){
-    this.router.navigate(['login']);
+    this.router.navigate(['']);
   }
 
 
