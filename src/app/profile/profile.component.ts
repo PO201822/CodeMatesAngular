397a5de935e030c0c1ff6899248f9cc8ee9bdd02
Gergő Papp <imgergopapp@gmail.com>
2019-08-14 11:58:04 +0200

    Implement profile component (html & .ts)

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -8,13 +8,38 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './profile.component.html'
 })
 export class ProfileComponent implements OnInit {
-  
+  username: string = 'username';
+  password: string = 'password';
+  email: string = 'email';
+  location: string = 'location';
+  address: string = 'address';
+
   constructor(
-    private http : HttpClient,
+    private http: HttpClient,
     private cookie: CookieService
   ) { }
 
   ngOnInit() {
+    let url = environment.apiUrl + '/profile';
+    this.http.post<any>(url, {
+      token: this.cookie.get('token')
+    }).subscribe(res => this.onResponseReceived(res),
+      error => this.handleError(error)); {
+    };
+  }
+
+  handleError(error: any): void {
+    console.log(error);
+
+  }
+
+  onResponseReceived(res) {
+    this.username = JSON.stringify(res.name);
+    this.password = JSON.stringify(res.password);
+    this.email = JSON.stringify(res.email);
+    this.location = JSON.stringify(res.location);
+    this.address = JSON.stringify(res.address);
+
   }
 
 }
