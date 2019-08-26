10f0cab18de112fc2619fbd42b429bbfe7941843
Taraszovics Péter <gtarapeti@gmail.com>
2019-08-26 14:13:00 +0200

    testing profile update

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { CookieService } from 'ngx-cookie-service';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html'
 })
 export class ProfileComponent implements OnInit {
+  id : number;
   username: string = 'username';
   password: string = 'password';
   email: string = 'email';
   location: string = 'location';
   address: string = 'address';
+  roles : any;
+  cut : number = 0;
+  premium :boolean = false;
+  profit : number = 0;
 
   constructor(
     private http: HttpClient,
@@ -34,12 +40,37 @@ export class ProfileComponent implements OnInit {
   }
 
   onResponseReceived(res) {
-    this.username = JSON.stringify(res.name);
-    this.password = JSON.stringify(res.password);
-    this.email = JSON.stringify(res.email);
-    this.location = JSON.stringify(res.location);
-    this.address = JSON.stringify(res.address);
+    this.id = res.id;
+    this.username = res.name;
+    this.password = res.password;
+    this.email = res.email;
+    this.location = res.location;
+    this.address = res.address;
+    this.roles = res.roles;
 
+  }
+
+  onUpdateClicked() {
+    let url = environment.apiUrl + '/profile';
+    this.http.put<any>(url, {
+      id: this.id,
+      name : this.username,
+      password : this.password,
+      email : this.email,
+      location : this.location,
+      address : this.address,
+      roles : this.roles,
+      cut : this.cut,
+      premium : this.premium,
+      profit : this.profit
+    }).subscribe(res => this.onProfileUpdateResponse(res),
+      error => this.handleError(error)); {
+    };
+    
+
+  }
+  onProfileUpdateResponse(res: any): void {
+    console.log('okcs');
   }
 
 }
