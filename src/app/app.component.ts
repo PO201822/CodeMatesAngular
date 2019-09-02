8b02aa1bae8cf55bf28f4b084554f04c6034d2cd
Gergő Papp <imgergopapp@gmail.com>
2019-09-02 18:26:20 +0200

    Add message service instance for all components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
+import { MessageService } from './services/message.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[MessageService]
 })
 
 export class AppComponent {
@@ -12,7 +14,8 @@ export class AppComponent {
   title = 'base-angular';
 
   constructor(
-    private cookie: CookieService
+    private cookie: CookieService,
+    private messageService : MessageService
   ) { }
 
   isCookieSet() {
