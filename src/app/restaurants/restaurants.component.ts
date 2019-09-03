142e00c0f0bef735bf9ba76213aad5e81b492e4d
Gergő Papp <imgergopapp@gmail.com>
2019-09-03 17:16:26 +0200

    Refactored restaurants component

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
+import { MessageService } from '../services/message.service';
 
 
 @Component({
@@ -11,20 +11,21 @@ import { CookieService } from 'ngx-cookie-service';
 })
 export class RestaurantsComponent implements OnInit {
 
-  model : any = {};
-  restaurantModel : any = [];
+  model: any = {};
+  restaurants: any = [];
+  location: string = '';
 
-  location : string = 'Miskolc';
-
-  constructor(private http : HttpClient,
-    private cookie: CookieService) { }
+  constructor(private http: HttpClient,
+    private cookie: CookieService,
+    private messageService: MessageService) { }
 
   ngOnInit() {
+    Promise.resolve(null).then(() => this.messageService.hideMessage());
     this.getCurrentLocation();
   }
 
 
-  getCurrentLocation(){
+  getCurrentLocation() {
     let url = environment.apiUrl + '/profile';
     this.http.post<any>(url, {
       token: this.cookie.get('token')
@@ -33,24 +34,29 @@ export class RestaurantsComponent implements OnInit {
     };
   }
 
-  onLocationRecieved(res){
-    this.location = JSON.stringify(this.location);
-    console.log(this.location);
+  onLocationRecieved(res) {
+    this.location = res.location;
   }
 
   handleError(error: any): void {
     console.log("No such location")
   }
 
-  onRestaurantsBySetLocationResponse(res){
-    this.restaurantModel = res;
+  onSearchByLocationResponse(res) {
+    this.restaurants = res;
+    if (this.restaurants.length == 0) {
+      this.messageService.showMessage("No restaurants located at '" + this.location + "'.", "info");
+    }
+    else{
+      this.messageService.hideMessage();
+    }
   }
 
-  onUseSetLocationClicked() {
+  onSearchByLocationClicked() {
     let url = environment.apiUrl + '/restaurants';
     let params = new HttpParams().set("location", this.location);
-    this.http.get(url, { params : params }).subscribe(res => this.onRestaurantsBySetLocationResponse(res),
-  error => this.handleError(error)); { };
+    this.http.get(url, { params: params }).subscribe(res => this.onSearchByLocationResponse(res),
+      error => this.handleError(error)); { };
   }
 
 }
