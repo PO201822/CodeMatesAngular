eb58fe815c86aa5d0aec1f9587d1a0c62168ad56
Taraszovics Péter <gtarapeti@gmail.com>
2019-09-10 09:12:18 +0200

    added test button

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MessageService } from '../services/message.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +8,13 @@ import { MessageService } from '../services/message.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private messageService: MessageService) { }
+  constructor(private messageService: MessageService, private client: HttpClient) { }
 
   ngOnInit() {
     Promise.resolve(null).then(() => this.messageService.hideMessage());
   }
 
+  doTest() {
+    this.client.get("http://localhost:8080/auth/me").subscribe(console.log);
+  }
 }
