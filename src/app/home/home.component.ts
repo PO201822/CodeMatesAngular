b8df0898be31e899756d33fac2b9970add6d647b
Taraszovics Péter <gtarapeti@gmail.com>
2019-09-10 13:38:27 +0200

    removed test button

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,8 +13,4 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     Promise.resolve(null).then(() => this.messageService.hideMessage());
   }
-
-  doTest() {
-    this.client.get("http://localhost:8080/auth/me").subscribe(console.log);
-  }
 }
