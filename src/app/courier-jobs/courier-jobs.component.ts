a3a7abc46814444c1d7e5ec7f475abd098c4dddd
Taraszovics Péter <gtarapeti@gmail.com>
2019-09-12 11:13:33 +0200

    implemented listing all orders that needs to be processed

--- src/app/courier-jobs/courier-jobs.component.ts
+++ src/app/courier-jobs/courier-jobs.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../services/message.service';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-courier-jobs',
@@ -6,9 +9,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourierJobsComponent implements OnInit {
 
-  constructor() { }
+  jobs : any = null;
+
+  constructor(
+    private messageService : MessageService,
+    private http: HttpClient
+  ) { }
 
   ngOnInit() {
+    Promise.resolve(null).then(() => this.messageService.hideMessage());
+    this.getAllJobs();
+
   }
+
+  getAllJobs() {
+    let url = environment.apiUrl + '/courier/getAllJobs';
+    this.http.get(url).subscribe(res => this.setAllJobs(res),
+      error => this.handleError(error)); { };
+  }
+
+
+  handleError(error: any): void {
+    console.log(error);
+  }
+
+  setAllJobs(res){
+    console.log(res)
+    this.jobs = res;
+  }
+
+  onPickUpCartClicked(cartId){
+    let url = environment.apiUrl + '/courier/pickUpJob';
+    this.http.post<any>(url, {
+      cartId : cartId
+    }).subscribe(res => this.onPickedUpCartClickedResponse(res),
+      error => this.handleError(error)); {
+    };
+
+  }
+  onPickedUpCartClickedResponse(res){
+    console.log("jo");
+  }
+
 
 }
