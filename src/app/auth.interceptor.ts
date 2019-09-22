36935aa44039efab30af90fad8b05281522789bb
Gergő Papp <imgergopapp@gmail.com>
2019-09-22 20:20:02 +0200

    Move (response) error handling to components

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -3,13 +3,14 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CookieService } from 'ngx-cookie-service';
 import { Router } from '@angular/router';
-import { tap, catchError } from 'rxjs/operators';
 
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-    constructor(private cookie: CookieService, private router : Router) {}
+    constructor(
+      private cookie: CookieService, 
+      private router : Router) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req.clone({
@@ -18,27 +19,6 @@ export class AuthInterceptor implements HttpInterceptor {
           'Accept'       : 'application/json',
           'Authorization': `Bearer ${this.cookie.get('token')}`,
         }
-      })).pipe(
-        tap(
-          event => this.handleResponse(req, event),
-          error => this.handleError(req, error)
-        )
-      );
-    }
-
-    handleResponse(req: HttpRequest<any>, event) {
-      console.log('Handling response for ', req.url, event);
-      if (event instanceof HttpResponse) {
-        console.log('Request for ', req.url,
-            ' Response Status ', event.status,
-            ' With body ', event.body);
-      }
-    }
-  
-    handleError(req: HttpRequest<any>, error) {
-      if(error.status == 403){
-        console.error('Unauthorized request');
-        this.router.navigate(["/unauthorized"]);
-      }
+      }));
     }
   }
