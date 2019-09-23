03962dc1beb1bb76f4708572f023476649688dda
Taraszovics Péter <gtarapeti@gmail.com>
2019-09-23 14:04:59 +0200

    implemented table and model

--- src/app/courier-own-orders/courier-own-orders.component.ts
+++ src/app/courier-own-orders/courier-own-orders.component.ts
@@ -3,6 +3,7 @@ import { MessageService } from '../services/message.service';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { ErrorHandlerService } from '../services/error-handler.service';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-courier-own-orders',
@@ -11,11 +12,13 @@ import { ErrorHandlerService } from '../services/error-handler.service';
 export class CourierOwnOrdersComponent implements OnInit {
 
   private jobs = null;
+  private closeResult : any;
 
   constructor(
     private messageService : MessageService,
     private http: HttpClient,
-    private errorHandlerService : ErrorHandlerService
+    private errorHandlerService : ErrorHandlerService,
+    private modalService: NgbModal,
   ) { }
 
   ngOnInit() {
@@ -31,6 +34,36 @@ export class CourierOwnOrdersComponent implements OnInit {
 
   setJobsList(res){
     this.jobs = res;
+
+    console.log(this.jobs);
+  }
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title', size: 'xl', scrollable: true }).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
+
+  onJobCompleteClicked(orderId){
+    let url = environment.apiUrl + '/courier/completeOrder';
+    this.http.put<any>(url, {
+      orderId : orderId
+    }).subscribe(res => this.messageService.showMessage("Profile updated successfuly.", "success"),
+      error => this.errorHandlerService.handleError(error)); {
+    };
+
   }
 
 }
