c1e4f295b2dbc45ec5180e0d7a708ebbad6bfbcf
Gergő Papp <imgergopapp@gmail.com>
2019-09-24 08:56:08 +0200

    Create courier home component

---
+++ src/app/courier-home/courier-home.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-courier-home',
+  templateUrl: './courier-home.component.html'
+})
+export class CourierHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
