b21de2595b7c61c63a1f4fd4f43c9862cacbc92c
Gergő Papp <imgergopapp@gmail.com>
2019-09-26 21:25:40 +0200

    Home component changes based on role

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MessageService } from '../services/message.service';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { ErrorHandlerService } from '../services/error-handler.service';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +10,26 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private messageService: MessageService, private client: HttpClient) { }
+  private role;
+
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient,
+    private errorHandlerService: ErrorHandlerService
+    ) { }
 
   ngOnInit() {
     Promise.resolve(null).then(() => this.messageService.hideMessage());
+    this.getUserRole();
+  }
+
+  getUserRole(){
+    let url = environment.apiUrl + '/getRole';
+    this.http.get(url).subscribe(res => this.setRole(res),
+      error => this.errorHandlerService.handleError(error)); { };
+  }
+
+  setRole(res) {
+    this.role = res.role;
   }
 }
