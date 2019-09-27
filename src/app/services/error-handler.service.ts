f02a542faf42a37efee4c5247ecec687e08f3f0f
Gergő Papp <imgergopapp@gmail.com>
2019-09-27 09:16:11 +0200

    Add loading screen hide

--- src/app/services/error-handler.service.ts
+++ src/app/services/error-handler.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
 import { MessageService } from './message.service';
 import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ErrorHandlerService {
 
-  constructor(private messageService: MessageService,
-    private router: Router) { }
+  constructor(
+    private messageService: MessageService,
+    private router: Router,
+    private spinner : NgxSpinnerService
+  ) {}
 
   handleError(error) {
+    this.spinner.hide();
     this.messageService.hideMessage();
     if (error.status == 403) {
       this.router.navigate(["/unauthorized"]);
