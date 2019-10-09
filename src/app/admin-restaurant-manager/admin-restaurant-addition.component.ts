243ea0e27e02a121cfad9cc0b78953d10635ab7b
Gergő Papp <imgergopapp@gmail.com>
2019-10-09 23:26:34 +0200

    Refactor & rename admin-restaurant-addition (to admin-restaurant-manager)

--- src/app/admin-restaurant-manager/admin-restaurant-addition.component.ts
+++ src/app/admin-restaurant-manager/admin-restaurant-addition.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { CookieService } from 'ngx-cookie-service';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { UserService } from '../services/user.service';
 import { MessageService } from '../services/message.service';
 import { ErrorHandlerService } from '../services/error-handler.service';
+
 
 @Component({
   selector: 'app-admin-restaurant-addition',
@@ -17,20 +18,51 @@ export class AdminRestaurantAdditionComponent implements OnInit {
   location: string = 'location';
   description: string = 'description';
 
+  restaurants: any;
+  restaurantCount = 0;
+
   constructor(
     private http: HttpClient,
     private cookie: CookieService,
     private userService: UserService,
     private messageService: MessageService,
-    private errorHandlerService : ErrorHandlerService
+    private errorHandlerService: ErrorHandlerService
   ) { }
 
   ngOnInit() {
     Promise.resolve(null).then(() => this.messageService.hideMessage());
+    this.getAllRestaurants();
   }
 
-  onUpdateClicked() {
-    if (!this.userService.isInputValid([this.description, this.name, this.picture, this.location])) {
+  getAllRestaurants() {
+    let url = environment.apiUrl + '/admin/listAllRestaurants';
+    this.http.get<any>(url)
+      .subscribe(res => this.setRestaurants(res),
+        error => this.errorHandlerService.handleError(error)); {
+    };
+  }
+
+  setRestaurants(res) {
+    this.restaurants = res;
+    this.restaurantCount = res.length + 1;
+  }
+
+  removeRestaurant(id) {
+    let url = environment.apiUrl + '/admin/removeRestaurant';
+    let params = new HttpParams().set('id',id);
+    this.http.delete(url, {params:params}).subscribe(res => this.getAllRestaurants(),
+      error => this.errorHandlerService.handleError(error)); { };
+  }
+
+  // onDeleteItemClicked(productId) {
+  //   let url = environment.apiUrl + '/deleteItem';
+  //   let params = new HttpParams().set("productId", productId);
+  //   this.http.delete(url, { params: params }).subscribe(res => this.getMyCart(),
+  //     error => this.errorHandlerService.handleError(error)); { };
+  // }
+
+  createRestaurant() {
+    if (!this.userService.isInputValid([this.name, this.location])) {
       this.messageService.showMessage("Every input field is required!", "danger");
       return;
     }
@@ -41,7 +73,7 @@ export class AdminRestaurantAdditionComponent implements OnInit {
       picture: this.picture,
       location: this.location,
       description: this.description
-    }).subscribe(res => this.messageService.showMessage("Restaurant successfuly added.", "success"),
+    }).subscribe(res => this.getAllRestaurants(),
       error => this.errorHandlerService.handleError(error)); {
     };
   }
